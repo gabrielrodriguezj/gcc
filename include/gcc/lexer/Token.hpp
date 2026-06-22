e0162b206d577ade2cde5b3a4fdd28b08a15b0f7
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2026-06-22 01:23:23 -0600

    Update Token.hpp. Token name printing

--- include/gcc/lexer/Token.hpp
+++ include/gcc/lexer/Token.hpp
@@ -36,4 +36,46 @@ typedef struct {
     int line;
 } Token;
 
+// Function to get the string representation
+constexpr std::string_view to_string(TokenName tokenName) {
+    switch (tokenName) {
+        case TokenName::LEFT_PAREN:     return "'('";
+        case TokenName::RIGHT_PAREN:    return "')'";
+        case TokenName::LEFT_BRACE:     return "'{'";
+        case TokenName::RIGHT_BRACE:    return "'}'";
+        case TokenName::COMMA:          return "','";
+        case TokenName::DOT:            return "'.'";
+        case TokenName::MINUS:          return "'-'";
+        case TokenName::PLUS:           return "'+'";
+        case TokenName::SEMICOLON:      return "';'";
+        case TokenName::SLASH:          return "'/'";
+        case TokenName::STAR:           return "'*'";
+        case TokenName::BANG:           return "'!'";
+        case TokenName::BANG_EQUAL:     return "'!='";
+        case TokenName::EQUAL:          return "'='";
+        case TokenName::EQUAL_EQUAL:    return "'=='";
+        case TokenName::GREATER:        return "'>'";
+        case TokenName::GREATER_EQUAL:  return "'>='";
+        case TokenName::LESS:           return "'<'";
+        case TokenName::LESS_EQUAL:     return "'<='";
+        case TokenName::IDENTIFIER:     return "'identifier'";
+        case TokenName::STRING:         return "'string'";
+        case TokenName::NUMBER:         return "'number'";
+        case TokenName::AND:            return "'&&'";
+        case TokenName::ELSE:           return "'else'";
+        case TokenName::FALSE:          return "'false'";
+        case TokenName::FOR:            return "'for'";
+        case TokenName::IF:             return "'if'";
+        case TokenName::OR:             return "'||'";
+        case TokenName::RETURN:         return "'return'";
+        case TokenName::TRUE:           return "'true'";
+        case TokenName::WHILE:          return "'while'";
+        case TokenName::INT:          return "'int'";
+        case TokenName::VOID:          return "'void'";
+        case TokenName::END:          return "'$'";
+        case TokenName::ERROR:          return "'error'";
+        default:                        return "Unknown";
+    }
+}
+
 #endif //GCC_TOKEN_HPP
